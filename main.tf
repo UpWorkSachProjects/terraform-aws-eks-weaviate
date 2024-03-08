dbe04c423854e68be87b2b1f3e3005292231796b
Ritik Gupta <gritik95@gmail.com>
2024-03-08 17:49:49 +0530

    added arn

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  service_account_role_arn = "arn:aws:iam::975050058851:role/terraform"
   tags = {
     "eks_addon" = "ebs-csi"
     "terraform" = "true"
