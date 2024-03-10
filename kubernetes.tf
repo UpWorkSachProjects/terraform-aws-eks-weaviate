341ec9a73eac50453513452150e3082a0922539b
Ritik Gupta <gritik95@gmail.com>
2024-03-10 13:17:49 +0530

    remove duplicate declaration

--- kubernetes.tf
+++ kubernetes.tf
@@ -14,13 +14,13 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.9"
-}
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   load_config_file       = false
+#   version                = "~> 1.9"
+# }
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
